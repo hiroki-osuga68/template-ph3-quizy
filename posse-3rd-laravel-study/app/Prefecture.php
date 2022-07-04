34f2a5ed4ac31cbf61fb3a103d02f563014077da
hiroki-osuga68 <osugahi1006@keio.jp>
2022-07-04 17:14:31 +0900

    モデル関連の整形

--- posse-3rd-laravel-study/app/Prefecture.php
+++ posse-3rd-laravel-study/app/Prefecture.php
@@ -6,11 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Prefecture extends Model
 {
-    // ホワイトリストの指定・・指定したカラムに対してのみ、 create()やupdate() 、fill()が可能になる
     protected $fillable = [
         'name',
     ];
-    // ファットコントローラを防ぐために、モデルにメソッドを割り当てる
 
     // questionsテーブルとhasMany関係であることを定義
     public function questions()
