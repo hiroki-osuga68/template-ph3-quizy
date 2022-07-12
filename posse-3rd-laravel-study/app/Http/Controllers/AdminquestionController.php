24130cc21e68a3201c5152a32bde267d55ed99dd
hiroki-osuga68 <osugahi1006@keio.jp>
2022-07-12 23:10:52 +0900

    設問画像の追加、編集、削除ページの実装

---
+++ posse-3rd-laravel-study/app/Http/Controllers/AdminquestionController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+// モデルの読み込み
+use App\Prefecture;
+use App\Question;
+use Illuminate\Http\Request;
+// ファサード
+use Illuminate\Support\Facades\Storage;
+
+class AdminquestionController extends Controller
+{
+  //
+  public function index($prefecture_id)
+  {
+    // findはプライマリーキーの検索なため、外部キー検索はwhereを用いた
+    $questions = Question::where('prefecture_id', $prefecture_id)->get();
+    return view('admin/edit_question', compact('questions', 'prefecture_id'));
+  }
+
+  public function store(Request $request)
+  {
+    $question = new Question;
+    $prefecture_id = $request->prefecture_id;
+
+    // 画像データ保存:storage/app/public
+    // 画像データ読込:public/storage
+
+    // name属性が'question'のinputタグをファイル形式に、画像をstorage/app/public/imagesに保存
+    $image_path = $request->file('question')->store('public/temp');
+    str_replace('public/', 'storage/', $image_path); //追加
+
+    // 画像のファイル名をDB保存・・上記処理にて保存した画像に名前を付け、questionテーブルのimageカラムに格納
+    $question->image = basename($image_path);
+    $question->prefecture_id = $request->prefecture_id;
+
+    $question->save();
+
+    return redirect()->route('edit_question.index', ['id' => $prefecture_id]);
+  }
+
+  public function edit($id)
+  {
+    //レコードを検索
+    $question = Question::find($id);
+    return view('/admin/each_edit_question', compact('question'));
+  }
+
+  public function update(Request $request, $id)
+  {
+    $question = Question::find($id);
+    // 画像ファイルインスタンス取得
+    $image = $request->file('question');
+    // 現在の画像へのパスをセット
+    $image_path = $question->image;
+    if (isset($image)) {
+      // 現在の画像ファイルの削除
+      Storage::disk('public')->delete($image_path);
+      // 選択された画像ファイルを保存してパスをセット
+      $image_path = $image->store('public/temp');
+      // ファイル名は$temp_pathから"public/temp/"を除いたもの
+      $filename = str_replace('public/temp/', '', $image_path);
+      // データベースを更新
+      $question->update([
+        'image' => $filename,
+      ]);
+    }
+
+    $prefecture_id = $question->prefecture_id;
+
+    return redirect()->route('edit_question.index', ['id' => $prefecture_id]);
+  }
+
+  public function destroy(Request $request, $id)
+  {
+    //削除対象レコードを検索
+    $question = Question::find($id);
+    $prefecture_id = $request->prefecture_id;
+
+    $question->delete();
+    return redirect()->route('edit_question.index', ['id' => $prefecture_id]);
+  }
+}
--- posse-3rd-laravel-study/routes/web.php
+++ posse-3rd-laravel-study/routes/web.php
@@ -24,11 +24,22 @@ Route::get('/', 'QuizyController@index')->name('index');
 Route::get('/quiz/{id}', 'QuizyController@page')->name('page');
 
 // 管理画面
+// タイトル管理
 Route::prefix('edit_title')->group(function () {
-Route::get('/', 'AdmintitleController@index')->name('edit_title.index');
-Route::post('/', 'AdmintitleController@store')->name('edit_title.store');
-// 編集ルーティング
-Route::get('/edit/{id}','AdmintitleController@edit')->name('edit_title.edit');
-Route::post('/update/{id}','AdmintitleController@update')->name('edit_title.update');
-Route::post('/destroy/{id}','AdmintitleController@destroy')->name('edit_title.destroy');
+    Route::get('/', 'AdmintitleController@index')->name('edit_title.index');
+    Route::post('/', 'AdmintitleController@store')->name('edit_title.store');
+    // 編集ルーティング
+    Route::get('/edit/{id}', 'AdmintitleController@edit')->name('edit_title.edit');
+    Route::post('/update/{id}', 'AdmintitleController@update')->name('edit_title.update');
+    Route::post('/destroy/{id}', 'AdmintitleController@destroy')->name('edit_title.destroy');
+});
+
+// 設問管理
+Route::prefix('edit_question')->group(function () {
+    Route::get('/{id}', 'AdminquestionController@index')->name('edit_question.index');
+    Route::post('/{id}', 'AdminquestionController@store')->name('edit_question.store');
+    // 編集ルーティング
+    Route::get('/edit/{id}', 'AdminquestionController@edit')->name('edit_question.edit');
+    Route::post('/update/{id}', 'AdminquestionController@update')->name('edit_question.update');
+    Route::post('/destroy/{id}', 'AdminquestionController@destroy')->name('edit_question.destroy');
 });
